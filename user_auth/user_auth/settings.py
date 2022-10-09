f55818a6963e6bf6df4f37bb8cf2e7cdeb07a4af
Abhishek Kumar <59925018+abhi26699@users.noreply.github.com>
2022-10-09 20:43:44 +0530

    Update settings.py

--- user_auth/user_auth/settings.py
+++ user_auth/user_auth/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-lm)d8-je(^)l@ya^mpt3bl+h-)fehe97=o!zyr0hf_5(^!0%*r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
